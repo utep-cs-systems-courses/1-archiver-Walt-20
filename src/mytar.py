3fe33861a88d34168bbee940df842e7002f22f50
Walt <walt.c0des20@gmail.com>
2023-02-08 06:56:40 -0700

    Updated the script to run as desired

--- src/mytar.py
+++ src/mytar.py
@@ -5,11 +5,11 @@ import sys
 # check out write file as well and the while loop. Index slicing till we get all the buffer
 # do this ^
 
-def mytar(file_names, tar_names):
-    # create the tar file
-    tar_file = tar_names[0]
+
+def mytar(file_names, target_file):
+
     # open the tar_file for storing
-    tar_fd = os.open(tar_file, os.O_CREAT | os.O_WRONLY) 
+    tar_fd = os.open(target_file, os.O_CREAT | os.O_WRONLY) 
     # looking through all files provided
     for file_name in file_names:
         # setting the file descriptor equal to the # current file
@@ -25,32 +25,34 @@ def mytar(file_names, tar_names):
         # write header of to tar descriptor
         os.write(tar_fd, header)
         # write file contents to tar descriptor
-        os.write(tar_fd, file_content)
+        # os.write(tar_fd, file_content)
         # close current file descriptor
         os.close(fd)
     # close tar descriptor
     os.close(tar_fd)
     sys.stdout.write("success\n")
 
-def extract_tar(file_names):
-    # traverse the tar files 
-    for tar_name in file_names:  
-        tar_fd = os.open(tar_name, os.O_RDONLY)
-    # read its contents
-        file_info = os.stat(tar_name)
-        file_size = file_info.st_size
-        file_content = os.read(tar_fd, file_size).decode()  
-        sys.stdout.write(file_content)
-        os.close(tar_fd)
+def extract_tar(target_file):
+    # traverse the tar files
+    tar_name = target_file 
+    # open the tar file
+    tar_fd = os.open(tar_name, os.O_RDONLY)
+    file_info = os.stat(tar_name)
+    file_size = file_info.st_size
+    # read the file content
+    file_content = os.read(tar_fd, file_size).decode()
+    # use information found in headers to create files and write contents
+    sys.stdout.write(file_content)
+    os.close(tar_fd)
 
 if __name__ == '__main__':
     mode = sys.argv[1]
-    file_names = sys.argv[2:]
-    tar_names = sys.argv[3:]
+    target_file = sys.argv[-1]
     if mode == 'c':
-        mytar(file_names, tar_names)
+        file_names = sys.argv[2:-1]
+        mytar(file_names, target_file)
     elif mode == 'x':
-        extract_tar(file_names)
+        extract_tar(target_file)
     else:
         sys.stdout.write("Usage: mytar.py c|x <file1> <file2> ... <fileN>")
         sys.exit(1)
